d3d9f97bf4030ca03c9ee72934528dd3deee01f3
tadkr <tadribeiro@gmail.com>
2017-07-18 19:46:35 -0300

    Preparando o apk para receber o algoritmo

--- app/src/main/java/io/development/tymo/utils/AlgorithmFeedSearch.java
+++ app/src/main/java/io/development/tymo/utils/AlgorithmFeedSearch.java
@@ -179,10 +179,6 @@ public class AlgorithmFeedSearch {
             return -1;
         else if (start_date_time > start_date_time2 && orderByDateHour == true)
             return 1;
-        else if (start_date_time > start_date_time2 && time_left_to_end < 0 && time_left_to_end2 < 0)
-            return -1;
-        else if (start_date_time < start_date_time2 && time_left_to_end < 0 && time_left_to_end2 < 0)
-            return 1;
         else if (rank_points > rank_points2)
             return -1;
         else if (rank_points < rank_points2)
